40382131d9f16222ea0499f7b479bb19fb028aae
ofgonen <ofgonen@gmail.com>
2023-07-21 01:24:01 +0300

    chore(Barrel): add name space

--- Assets/Assigment_CreateRadialTrigger/Barrel.cs
+++ Assets/Assigment_CreateRadialTrigger/Barrel.cs
@@ -4,18 +4,21 @@ using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 
-public class Barrel : MonoBehaviour
+namespace Assignment.RadialTrigger
 {
-    public float trigger_radius = 1f;
-    public Transform trigger_target;
-    private void OnDrawGizmos()
+    public class Barrel : MonoBehaviour
     {
-        var distance = Vector3.Distance(transform.position, trigger_target.position);
-        var trigger = distance <= trigger_radius;
+        public float trigger_radius = 1f;
+        public Transform trigger_target;
+        private void OnDrawGizmos()
+        {
+            var distance = Vector3.Distance(transform.position, trigger_target.position);
+            var trigger = distance <= trigger_radius;
 
-        Handles.color = trigger ? Color.red : Color.green;
+            Handles.color = trigger ? Color.red : Color.green;
         
-        Handles.DrawWireArc(transform.position, Vector3.back, Vector3.left, 360f, trigger_radius);
-        Handles.Label(transform.position + Vector3.up * trigger_radius / 2f, "Trigger: " + (trigger ? "On" : "Off"));
-    }
+            Handles.DrawWireArc(transform.position, Vector3.back, Vector3.left, 360f, trigger_radius);
+            Handles.Label(transform.position + Vector3.up * trigger_radius / 2f, "Trigger: " + (trigger ? "On" : "Off"));
+        }
+    }    
 }
