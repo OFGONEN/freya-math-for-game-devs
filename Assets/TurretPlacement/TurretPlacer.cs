5a71ca8b8f9dfaa162ce287ffad87cd85ef4b84c
ofgonen <ofgonen@gmail.com>
2023-07-22 21:35:47 +0300

    feat(Example-Turret): place turret to surface along the camera's forward

---
+++ Assets/TurretPlacement/TurretPlacer.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class TurretPlacer : MonoBehaviour
+{
+    public Transform turret;
+
+    private void OnDrawGizmosSelected()
+    {
+        Debug.Log("On Draw Gizmos Selected");
+    }
+
+    private void OnDrawGizmos()
+    {
+        Ray ray = new Ray(transform.position, transform.forward);
+
+        Debug.Log("On Draw Gizmos");
+        if (Physics.Raycast(ray, out RaycastHit hit))
+        {
+            var up = hit.normal;
+            var right = Vector3.Cross(up, transform.forward).normalized;
+            var forward = Vector3.Cross(right, up);
+            
+            Gizmos.color = Color.green;
+            Gizmos.DrawRay(hit.point, up);
+
+            Gizmos.color = Color.red;
+            Gizmos.DrawRay(hit.point, right);
+            
+            Gizmos.color = Color.blue;
+            Gizmos.DrawRay(hit.point, forward);
+
+            turret.position = hit.point;
+            turret.rotation = Quaternion.LookRotation(forward, up);
+        }
+    }
+}
