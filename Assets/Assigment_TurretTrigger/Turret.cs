f34b20b2f15a022c53502be0130d2580b9575e59
ofgonen <ofgonen@gmail.com>
2023-07-23 05:25:33 +0300

    feat(Assignment-5.B): turret trigger with angle threshold

--- Assets/Assigment_TurretTrigger/Turret.cs
+++ Assets/Assigment_TurretTrigger/Turret.cs
@@ -11,9 +11,70 @@ public class Turret : MonoBehaviour
 
     public float trigger_height = 1f;
     public float trigger_radius = 1f;
+    [Range(0, 1)]
+    public float trigger_angle_threshold = 0.5f;
 
-    private void OnDrawGizmos() => OnTriggerWithHeigthAndRange();
+    public bool isInHeight, isInRadius, isInAngle;
     
+    private void OnDrawGizmos() => OnTriggerWithThreshold();
+
+    private void OnTest()
+    {
+        var offset = target.position - transform.position;
+        Handles.Label(target.position, "Angle Threshold: " + Vector3.Dot(transform.forward, offset.normalized) );
+    }
+        
+    //Assigment 5.B
+    private void OnTriggerWithThreshold()
+    {
+        if(!target) return;
+
+        var forward = transform.forward;
+        var right = transform.right;
+        var up = transform.up;
+        
+        var origin = transform.position;
+        var top = origin + up * trigger_height;
+
+        var targetInversedPosition = transform.InverseTransformPoint(target.position);
+        var targetInversedVector = transform.InverseTransformDirection(target.position - transform.position);
+        targetInversedVector.y = 0;
+        targetInversedVector = targetInversedVector.normalized;
+
+        isInHeight = targetInversedPosition.y > 0 && targetInversedPosition.y < trigger_height;
+        isInRadius =
+            (targetInversedPosition.x * targetInversedPosition.x + targetInversedPosition.z * targetInversedPosition.z) <
+            trigger_radius * trigger_radius;
+        isInAngle = trigger_angle_threshold < targetInversedVector.z;
+        bool isTrigger = isInHeight && isInRadius && isInAngle;
+
+        Handles.Label(target.position + Vector3.up / 2f, "Threshold: " + Vector3.Dot(forward, targetInversedPosition.normalized));
+        Handles.Label(target.position, "LocalPos: " + targetInversedPosition);
+        
+        var color = isTrigger ? Color.red : Color.green;
+
+        Handles.color = color;
+        Gizmos.color = color;
+        
+        Handles.DrawWireDisc(origin, up, trigger_radius);
+        Handles.DrawWireDisc(top, up, trigger_radius);
+
+        float p = trigger_angle_threshold;
+        float x = Mathf.Sqrt(1 - p * p);
+        Vector3 vRight = (forward * trigger_angle_threshold + right * x) * trigger_radius;
+        Vector3 vLeft = (forward * trigger_angle_threshold - right * x) * trigger_radius;
+        
+        Gizmos.DrawRay(origin, vRight);
+        Gizmos.DrawRay(origin, vLeft);
+        Gizmos.DrawRay(top, vRight);
+        Gizmos.DrawRay(top, vLeft);
+        
+        Gizmos.DrawLine(origin, top);
+        Gizmos.DrawLine(origin + vLeft, top + vLeft);
+        Gizmos.DrawLine(origin + vRight, top + vRight);
+    }
+    
+    //Assigment 5.A
     private void OnTriggerWithHeigthAndRange()
     {
         if (!target) return;
